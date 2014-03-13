d986e82c1e5b9986b52a0075fb5174324925cf11
Stephen Macke <smackee618@gmail.com>
2014-03-13 04:32:06 -0700

    update site root, merge Rakefile and Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,12 +1,51 @@
 require "rubygems"
 require "bundler/setup"
+require "jekyll"
 require "stringex"
+require "tmpdir"
+require "shellwords"
 
 ## -- Config -- ##
 
 posts_dir       = "_posts"    # directory for blog files
 new_post_ext    = "md"  # default new post file extension when using the new_post task
 new_page_ext    = "md"  # default new page file extension when using the new_page task
+GITHUB_REPONAME = "smacke/snapworld" # repo to publish to
+
+
+########################
+# Publish to gh-pages  #
+########################
+
+namespace :site do
+  desc "Compile less and minify css"
+  task :grunt do
+    sh grunt
+  end
+
+  desc "Generate blog files"
+  task :generate do
+    Jekyll::Site.new(Jekyll.configuration({
+      "source"      => ".",
+      "destination" => "_site"
+    })).process
+  end
+
+
+  desc "Generate and publish blog to gh-pages"
+  task :publish => [:generate] do
+    Dir.mktmpdir do |tmp|
+      cp_r "_site/.", tmp
+      Dir.chdir tmp
+      system "git init"
+      system "git add ."
+      message = "Site updated at #{Time.now.utc}"
+      system "git commit -m #{message.inspect}"
+      system "git remote add origin git@github.com:#{GITHUB_REPONAME}.git"
+      system "git push origin master:refs/heads/gh-pages --force"
+    end
+  end
+end
 
 
 #############################
